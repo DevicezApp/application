50af3d72d65d0c3c2e46ec43e0192c1416611363
elektr0nisch <contact@elektronisch.dev>
2023-05-17 12:00:26 +0200

    Add ASCII tables

--- server/src/main/java/de/devicez/server/console/command/DeviceCommand.java
+++ server/src/main/java/de/devicez/server/console/command/DeviceCommand.java
@@ -1,9 +1,12 @@
 package de.devicez.server.console.command;
 
+import com.github.freva.asciitable.AsciiTable;
+import com.github.freva.asciitable.Column;
 import de.devicez.server.DeviceZServerApplication;
 import de.devicez.server.networking.Client;
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.Arrays;
 import java.util.Collection;
 
 @Slf4j
@@ -29,11 +32,13 @@ public class DeviceCommand extends AbstractCommandHandler {
                 }
 
                 log.info("Connected clients ({}):", clients.size());
-                log.info("");
 
-                for (final Client client : clients) {
-                    log.info("{} ({}) running on {}", client.getName(), client.getId(), client.getPlatform());
-                }
+                final String table = AsciiTable.getTable(clients, Arrays.asList(
+                        new Column().header("Name").with(Client::getName),
+                        new Column().header("ID").with(client -> client.getId().toString()),
+                        new Column().header("Platform").with(client -> client.getPlatform().toString())
+                ));
+                log.info("\n" + table);
             }
         }
     }
