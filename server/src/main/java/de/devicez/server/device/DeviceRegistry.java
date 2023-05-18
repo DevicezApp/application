a255c53ee028383e467abcf3f381acd5f698a729
elektr0nisch <contact@elektronisch.dev>
2023-05-18 16:04:59 +0200

    Prevent the same device to be connected multiple times

--- server/src/main/java/de/devicez/server/device/DeviceRegistry.java
+++ server/src/main/java/de/devicez/server/device/DeviceRegistry.java
@@ -13,25 +13,42 @@ import java.util.*;
 public class DeviceRegistry {
 
     private final DeviceZServerApplication application;
-    private final Map<Long, ConnectedDevice> connectedDeviceMap = new HashMap<>();
+    private final Map<Long, ConnectedDevice> connectedDeviceSessionMap = new HashMap<>();
+    private final Map<UUID, ConnectedDevice> connectedDeviceIdMap = new HashMap<>();
 
     public DeviceRegistry(final DeviceZServerApplication application) {
         this.application = application;
     }
 
     public void handleDeviceConnect(final UUID id, final String name, final Platform platform, final byte[] hardwareAddress, final IStreamSession session) {
-        final ConnectedDevice device = new ConnectedDevice(application, id, name, platform, hardwareAddress, session);
-        connectedDeviceMap.put(session.getId(), device);
+        final ConnectedDevice newDevice = new ConnectedDevice(application, id, name, platform, hardwareAddress, session);
+
+        // Check if there is already a device with that id online
+        final ConnectedDevice connectedDevice = connectedDeviceIdMap.get(id);
+        if (connectedDevice != null) {
+            connectedDevice.getSession().quickClose();
+            log.warn("Device {} logged in from another location", name);
+        }
+
+        // Add to id map
+        connectedDeviceIdMap.put(id, newDevice);
+
+        // Add to session map
+        connectedDeviceSessionMap.put(session.getId(), newDevice);
         log.info("Device {} connected from {}", name, session.getRemoteAddress().toString());
 
         // Update information in database
-        application.getDatabaseClient().saveSerializable(device);
+        application.getDatabaseClient().saveSerializable(newDevice);
     }
 
     public void handleDeviceDisconnect(final long sessionId) {
-        final ConnectedDevice device = connectedDeviceMap.remove(sessionId);
+        // Remove from session map
+        final ConnectedDevice device = connectedDeviceSessionMap.remove(sessionId);
         device.setLastSeen(new Timestamp(System.currentTimeMillis()));
         log.info("Device {} disconnected", device.getName());
+
+        // Remove from id map
+        connectedDeviceIdMap.remove(device.getId());
 
         // Update information in database
         application.getDatabaseClient().saveSerializable(device);
@@ -43,13 +60,18 @@ public class DeviceRegistry {
     }
 
     public Device getDeviceById(final UUID id) {
+        final ConnectedDevice connectedDevice = connectedDeviceIdMap.get(id);
+        if (connectedDevice != null) {
+            return connectedDevice;
+        }
+
         return application.getDatabaseClient().readSerializable(Device.class, "id", id);
     }
 
     public Collection<Device> getAllDevices() {
         final List<Device> devices = application.getDatabaseClient().readSerializableList(Device.class);
         devices.removeIf(device -> {
-            for (final ConnectedDevice connectedDevice : connectedDeviceMap.values()) {
+            for (final ConnectedDevice connectedDevice : connectedDeviceSessionMap.values()) {
                 if (connectedDevice.getId().equals(device.getId())) {
                     return true;
                 }
@@ -61,10 +83,10 @@ public class DeviceRegistry {
     }
 
     public ConnectedDevice getConnectedDeviceBySession(final IStreamSession session) {
-        return connectedDeviceMap.get(session.getId());
+        return connectedDeviceSessionMap.get(session.getId());
     }
 
     public Collection<ConnectedDevice> getConnectedDevices() {
-        return connectedDeviceMap.values();
+        return connectedDeviceSessionMap.values();
     }
 }
